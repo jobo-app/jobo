0acdf8b472af7deab77a494e1d305c7b47a4d125
Kostas Karachalios <vrinek@me.com>
2013-10-19 22:37:49 +0200

    Fix a view spec

--- spec/views/jobs/index.html.erb_spec.rb
+++ spec/views/jobs/index.html.erb_spec.rb
@@ -16,8 +16,8 @@ describe "jobs/index" do
 
   it "renders a list of jobs" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Company Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Position Title".to_s, :count => 2
+
+    rendered.should have_css(".job", text: "Company Name", count: 2)
+    rendered.should have_css(".job", text: "Position Title", count: 2)
   end
 end
