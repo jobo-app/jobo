f226e913cc8a2147ca6d0d11452bc1d8d8cf419d
Kostas Karachalios <vrinek@me.com>
2013-10-19 14:14:25 +0200

    Add configs for the newly added gems

---
+++ spec/support/wait_for_ajax.rb
@@ -0,0 +1,15 @@
+def wait_for_ajax
+  Timeout.timeout(Capybara.default_wait_time) do
+    until page.evaluate_script('jQuery.active') == 0
+      sleep 0.1
+    end
+  end
+rescue Capybara::Poltergeist::JavascriptError
+  # Some tests don't visit any page
+end
+
+RSpec.configure do |config|
+  config.after :each, type: :feature do
+    wait_for_ajax
+  end
+end
