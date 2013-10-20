65998ce1c74c7e81d789262a9434d193271027bf
Kostas Karachalios <vrinek@me.com>
2013-10-20 17:53:07 +0200

    Fix typos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = GuestUser.create!
     session[:user_id] = user.id
-    flash[:success] = "Singed in as #{user.name}!"
+    flash[:success] = "Signed in as #{user.name}!"
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "Singed out."
+    flash[:success] = "Signed out."
     redirect_to root_path
   end
 end
