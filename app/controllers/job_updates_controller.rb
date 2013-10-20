3af49cb86f4ebe567d7120eb79ed42a89a6347ae
Kostas Karachalios <vrinek@me.com>
2013-10-20 13:26:23 +0200

    WIP

--- app/controllers/job_updates_controller.rb
+++ app/controllers/job_updates_controller.rb
@@ -2,7 +2,7 @@ class JobUpdatesController < ApplicationController
   include AngularController
 
   def index
-    @job_updates = JobUpdate.by_job_id(params[:job_id])
+    @job_updates = JobUpdate.by_job_id(params[:job_id]).newest_first
     respond_with @job_updates
   end
 
